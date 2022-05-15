9c363a22fc2edc8586c74f3ebd3948db83aff9d6
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-05-15 12:14:08 +0100

    some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "index#home"
+ 
+  get "/", to: "index#home"
+  get "/articleslist", to: "index#articles_list"
+  get "/article", to: "index#article"
 end
